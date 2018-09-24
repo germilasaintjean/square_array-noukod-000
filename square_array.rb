b8fd875618acacd8eecf92ce7be526837c3f596b
Saint-Jean Germila <germila@noukod.com>
2018-09-24 16:08:13 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
-def square_array(numbers)
-  numbers = []
-  numbers.each do |element |
-    numbers.push(element*element)
+def square_array(arr)
+  arr = []
+  arr.each do |element |
+    arr.push(element*element)
   end
-  numbers
+  arr
 end
